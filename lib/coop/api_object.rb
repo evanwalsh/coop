33d2da8c05c31e1e68e3ffc1a7c02c5bdc8163e5
Evan Walsh <evan@nothingconcept.com>
2012-03-08 14:02:55 -0500

    Load all APIObject

--- lib/coop/api_object.rb
+++ lib/coop/api_object.rb
@@ -1,6 +1,9 @@
 require 'hashie'
-Dir[File.expand_path('api_object/*.rb', __FILE__)].each{ |f| require f }
 
-class APIObject < Hashie::Mash
+module Coop
+  class APIObject < Hashie::Mash
+
+  end
   
+  %w(agenda group status user).each{ |o| require "coop/api_object/#{o}" }
 end
