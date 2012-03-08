88d28ee2b2a3ff03880e1b61d2f02cd22c533af1
Evan Walsh <evan@nothingconcept.com>
2012-03-08 14:44:09 -0500

    Add base_uri and defaults headers to all requests

--- lib/coop.rb
+++ lib/coop.rb
@@ -1,9 +1,11 @@
-require "coop/api_object"
-require "coop/session"
-require "coop/version"
+require 'coop/api_object'
+require 'coop/session'
+require 'coop/version'
 
 module Coop
   include HTTParty
+  base_uri 'http://coopapp.com'
+  headers "Accept" => "application/json"
   
   class << self
     def new(email, password)
