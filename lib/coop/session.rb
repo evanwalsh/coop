1d51d4f5012bc421e49b24dc3d6c2281b0237392
Evan Walsh <evan@nothingconcept.com>
2012-04-01 21:08:11 -0400

    Add /groups fetching && tests

--- lib/coop/session.rb
+++ lib/coop/session.rb
@@ -1,7 +1,7 @@
 module Coop
   class Session
     attr_accessor :email
-    
+
     # Public: Creates a new API session
     #
     # email - (String) the user's email
@@ -17,7 +17,7 @@ module Coop
       @email = email
       password = password
     end
-    
+
     # Public: Creates a Coop::Group object
     #
     # id - (Fixnum) the group id (as found in the app URL: http://coopapp.com/groups/[group_id])
@@ -33,7 +33,7 @@ module Coop
         id: id
       })
     end
-    
+
     # Public: Creates a Coop::Group object
     #
     # id - (Fixnum) the group id (as found in the app URL: http://coopapp.com/groups/[group_id]/users/[user_id])
@@ -46,6 +46,18 @@ module Coop
     # Returns a Coop::User instance
     def user(id)
       Coop::User.find(id)
+    end
+
+    # Public: List of all the groups a user belongs to
+    #
+    # Examples
+    #
+    #   session.groups
+    #   # => [#<Coop::APIObject>, #<Coop::APIObject>, ...]
+    #
+    # Returns Array of APIObjects with group data
+    def groups
+      Coop.get_parsed("/groups")
     end
   end
 end
--- test/coop/test_session.rb
+++ test/coop/test_session.rb
@@ -4,13 +4,23 @@ class TestSession < MiniTest::Unit::TestCase
   def setup
     @coop = Coop.new("test@example.com", "password")
   end
-  
+
   def test_new_is_session
     assert_instance_of Coop::Session, @coop
   end
-  
+
   def test_group
     @group = @coop.group(12345)
     assert_instance_of Coop::Group, @group
+  end
+
+  def test_groups
+    stub_get("/groups").with({
+      headers: { 'Accept' => 'application/json' }
+    }).to_return({
+      body: fixture('groups.json')
+    })
+
+    assert_instance_of Array, @coop.groups
   end
 end
